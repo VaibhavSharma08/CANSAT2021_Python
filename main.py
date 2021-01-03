4b2832161be8a45a590ce3a57337778952995a1f
Vaibhav Sharma <62774848+VaibhavSharma08@users.noreply.github.com>
2021-01-03 20:13:30 +0530

    Integrated Code

---
+++ main.py
@@ -0,0 +1,162 @@
+import matplotlib.animation as animation
+import numpy as np
+import pandas as pd
+from matplotlib.pyplot import show, subplots
+import serial
+import time
+import csv
+import threading
+from matplotlib import style
+import sys
+import largeList
+
+"""
+ISSUES: 
+1. Agar arduino se delay toh not writing to CSV
+2. If no delay then pandas not reading from created CSV of reader()
+3. When reader() creating CSV toh pandas cannot access
+4. If no file already then CSV is created after script stops
+5. TO DO: Pandas reading from stored list as a variable and plotting
+6. Dynamically access CSV for read write -> tailf can be used
+7. Arduino se data veryyyy inconsistent
+"""
+
+column, count_time, figure, axes = None, None, None, None
+x_lim, store, flag, check, y, df, dataList = None, None, None, None, None, None, None
+
+
+def initialise():
+    global column, count_time, figure, axes, x_lim, store, flag, check, y, dataList
+
+    dataList = []
+    x_lim = []
+    store = 0
+    flag = 1
+    check = []
+    count_time = 0
+
+    column = np.array(['TEMPERATURE', 'ALTITUDE', 'AVG SPEED', 'PRESSURE'])
+    figure, axes = subplots(nrows=1, ncols=4, figsize=(22, 5))
+    figure.suptitle('Data Plots', fontsize=24)
+    style.use('fivethirtyeight')
+
+    y = {'TEMPERATURE': [], 'ALTITUDE': [], 'AVG SPEED': [], 'PRESSURE': []}
+
+
+def axesLabel(i):
+    global flag, store, axes
+
+    if i == 1:
+        axes[0].set_xlim(flag, flag + store)
+        axes[1].set_xlim(flag, flag + store)
+        axes[2].set_xlim(flag, flag + store)
+        axes[3].set_xlim(flag, flag + store)
+
+    if i == 2:
+        axes[0].set_ylabel('Temperature (C)')
+        axes[1].set_ylabel('Altitude (m)')
+        axes[2].set_ylabel('Avg Speed (m/s)')
+        axes[3].set_ylabel('Pressure (Pa)')
+
+
+def plotter(index):
+    global y, df, column, count_time, axes, dataList
+    l = list(map(int, dataList[-1].split(',')))
+
+    y[column[index]].append(l[index])
+    x = np.arange(count_time)
+
+    axes[index].clear()
+    axes[index].plot(x, y[column[index]])
+    axes[index].set_xlabel('Time (s)')
+
+
+def animate(frame):
+    global store, count_time, df, flag
+    df = pd.read_csv('writtenData.csv')
+    count_time += 1
+    store += 1
+
+    try:
+        for index in range(4):
+            plotter(index)
+    except:
+        sys.exit()
+
+    if 35 < store < 100:
+        axesLabel(1)
+        flag += 1
+    elif store > 100:  # Logic
+        sys.exit()
+
+    axesLabel(2)
+
+
+def reader():
+    ser = serial.Serial(port='COM9', baudrate=9600, bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE, timeout=3)
+
+    time.sleep(3)
+    fieldnames = ["TEMPERATURE", "ALTITUDE", "AVG SPEED", "PRESSURE"]
+    # datafile1 = open('writtenData.csv', 'w')
+    # datafile1.write("TEMPERATURE,ALTITUDE,AVG SPEED,PRESSURE\n")
+    dataList.append("TEMPERATURE,ALTITUDE,AVG SPEED,PRESSURE")
+    with open('writtenData.csv', 'a') as datafile1:
+        csv_writer = csv.DictWriter(datafile1, fieldnames=fieldnames)
+        #csv_writer.writerow(fieldnames)
+        try:
+            ser.isOpen()
+            print("Serial port is open")
+        except:
+            print("Error - Serial Port not Open")
+            exit()
+
+        t = 1
+
+        if ser.isOpen():
+            try:
+                while True:
+                    time.sleep(1)
+                    data = ser.readline().decode('ascii')
+                    if t == 1:
+                        t = 0
+                        dataList.append(data[1:-2])
+                        l = list(map(int, data[1:-2].split(',')))
+                        print(l)
+                        info = {
+                            "TEMPERATURE": l[0],
+                            "ALTITUDE": l[1],
+                            "AVG SPEED": l[2],
+                            "PRESSURE": l[3],
+                        }
+                    else:
+                        dataList.append(data[:-2])
+                        l = list(map(int, data[:-2].split(',')))
+                        info = {
+                            "TEMPERATURE": l[0],
+                            "ALTITUDE": l[1],
+                            "AVG SPEED": l[2],
+                            "PRESSURE": l[3],
+                        }
+                    print(info)
+                    csv_writer.writerow(info)
+                    # print("abcdef")
+                    # print(dataWrite[:-1])
+                    # datafile.writerow(dataWrite)
+            except Exception:
+                print(dataList)
+                print("Error - Not able to write data")
+        else:
+            print("Cannot Open Serial Port")
+
+
+if __name__ == '__main__':
+    initialise()
+    thread = threading.Thread(target=reader)
+    thread.start()
+#    print("abc")
+#    print(sys.maxsize)
+#    time.sleep(7)
+#    plot = animation.FuncAnimation(figure, animate, interval=1000)
+#    figure.tight_layout(pad=2)
+#    show()
