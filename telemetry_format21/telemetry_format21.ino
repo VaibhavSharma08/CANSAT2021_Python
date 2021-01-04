452880d65e20eb547b9e57fb19fc827c7fa4f26f
Vaibhav Sharma <62774848+VaibhavSharma08@users.noreply.github.com>
2021-01-04 16:51:15 +0530

    Arduino code updated for packet format according to Mission Guide

--- telemetry_format21/telemetry_format21.ino
+++ telemetry_format21/telemetry_format21.ino
@@ -7,33 +7,37 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
     i++;
+    Serial.print("4800,1234567,");
+    Serial.print(i);
+    Serial.print(",12,0,0,0,");
     if(i==1){
-    Serial.println("74,77,2,119");
+    Serial.print("74,77,2,119");
     }
     if(i==2){
-    Serial.println("43,85,119,13");
+    Serial.print("43,85,119,13");
     }
     if(i==3){
-    Serial.println("172,18,48,97");
+    Serial.print("172,18,48,97");
     }
     if(i==4){
-    Serial.println("23,3,8,48");
+    Serial.print("23,3,8,48");
     }
     if(i==5){
-    Serial.println("31,32,143,43");
+    Serial.print("31,32,143,43");
     }
     if(i==6){
-    Serial.println("96,34,127,80");
+    Serial.print("96,34,127,80");
     }
     if(i==7){
-    Serial.println("2,52,104,25");
+    Serial.print("2,52,104,25");
     }
     if(i==8){
-    Serial.println("115,24,68,56");
+    Serial.print("115,24,68,56");
     }
     if(i==9){
-    Serial.println("76,40,42,18");
+    Serial.print("76,40,42,18");
     i = 0;
     }   
+    Serial.println("0,0,0,0,0,0,0,0,0");
     delay(1000);
 }
